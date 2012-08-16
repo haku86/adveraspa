6bdbfd544b06d1ec2d3464ff9623304e58daa7d1
Joel Tan <lomyril86@gmail.com>
2012-08-16 23:59:11 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adveraspa::Application.config.secret_token = 'ad69896065ea4a5451eb62a2bfa6fda72f6bba78a3b2933aa88e5a314b0ad3da2e29f40210ddfa6cf7bc1eb269ad68cf0998c39f363eede7844a5ee47af5c215'
